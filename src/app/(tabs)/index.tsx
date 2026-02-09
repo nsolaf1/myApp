750ba6ef4f871552866978b03f69dfcc09117a82
NSOLAf1 <nsolaf1@gmail.com>
2026-02-09 14:11:51 +0600

    l3

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,12 @@
+import Greeting from "@/components/Greeting";
 import { View } from "react-native";
 
 export default function HomeScreen() {
-  return <View style={{ flex: 1, backgroundColor: "#fff" }}></View>;
+  return <View style={{ flex: 1, backgroundColor: "#fff" }}>
+    <Greeting name="Nasirdin" role="teacher"/>
+
+
+    <ProfileCard name="student_name" role="student_role" course="course_name" bio="student_info"/>
+ 
+  </View>;
 }
