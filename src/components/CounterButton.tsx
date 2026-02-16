da6781cb639110c1aeb7923464681a8231f90dc9
NSOLAf1 <nsolaf1@gmail.com>
2026-02-16 14:27:30 +0600

    err

--- src/components/CounterButton.tsx
+++ src/components/CounterButton.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Button, Text, View } from "react-native";
 
-export default function CounterButton( {title, initialValue, step}) {
+export default function CounterButton( {title}) {
   const [count, setCount] = useState(initialValue);
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
